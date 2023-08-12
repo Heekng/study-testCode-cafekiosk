8d49fd31f8cadadb38fe3fb7b4627374747d3daf
heekng <rhkd1769@gmail.com>
2023-08-13 01:10:10 +0900

    Presentation Layer 테스트 1

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,26 +1,62 @@
 package sample.cafekiosk.spring.api.service.product;
 
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.forDisplay;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
-import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
+/**
+ * <b>readOnly = true : 읽기전용 CRUD</b>
+ * <p>CRUD 에서 CUD 동작 X / only read</p>
+ * <p>JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)</p>
+ * <p></p>
+ * <p>CQRS - Command / Query</p>
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // 동시성 이슈
+    // UUID
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // nextProductNumber
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
-        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
 
         return products.stream()
             .map(ProductResponse::of)
             .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 
 }
