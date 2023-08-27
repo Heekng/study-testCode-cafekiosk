98bfc2a16fd498aa91531110f16e410e54fceeb6
heekng <rhkd1769@gmail.com>
2023-08-27 15:22:52 +0900

    @DynamicTest

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -3,14 +3,19 @@ package sample.cafekiosk.spring.domain.product;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import sample.cafekiosk.spring.domain.stock.Stock;
 
 class ProductTypeTest {
 
@@ -103,6 +108,41 @@ class ProductTypeTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("")
+    @TestFactory
+    Collection<DynamicTest> dynamicTest() {
+        return List.of(DynamicTest.dynamicTest("", () -> {
+
+        }), DynamicTest.dynamicTest("", () -> {
+
+        }));
+    }
+
+    @DisplayName("재고 차감 시나리오")
+    @TestFactory
+    Collection<DynamicTest> stockDeductionDynamicTest() {
+        // given
+        Stock stock = Stock.create("001", 1);
+
+        return List.of(DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+            // given
+            int quantity = 1;
+
+            // when
+            stock.deductQuantity(quantity);
+
+            // then
+            assertThat(stock.getQuantity()).isZero();
+        }), DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+            // given
+            int quantity = 1;
+
+            // when // then
+            assertThatThrownBy(() -> stock.deductQuantity(quantity)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차감할 재고 수량이 없습니다.");
+        }));
     }
 
 }
