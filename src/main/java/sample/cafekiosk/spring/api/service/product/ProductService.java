5c822091eee13390177a0f60de5607ab40618243
heekng <rhkd1769@gmail.com>
2023-08-27 21:23:10 +0900

    private 메서드의 테스트

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductNumberFactory;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 /**
@@ -25,13 +26,14 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductNumberFactory productNumberFactory;
 
     // 동시성 이슈
     // UUID
     @Transactional
     public ProductResponse createProduct(ProductCreateServiceRequest request) {
         // nextProductNumber
-        String nextProductNumber = createNextProductNumber();
+        String nextProductNumber = productNumberFactory.createNextProductNumber();
 
         Product product = request.toEntity(nextProductNumber);
         Product savedProduct = productRepository.save(product);
@@ -45,18 +47,6 @@ public class ProductService {
         return products.stream()
             .map(ProductResponse::of)
             .collect(Collectors.toList());
-    }
-
-    private String createNextProductNumber() {
-        String latestProductNumber = productRepository.findLatestProductNumber();
-        if (latestProductNumber == null) {
-            return "001";
-        }
-
-        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
-        int nextProductNumberInt = latestProductNumberInt + 1;
-
-        return String.format("%03d", nextProductNumberInt);
     }
 
 }
